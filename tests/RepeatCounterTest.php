affe1d9a1875dabfa6197da7912972900e1fca3d
Kyle <student@epicodus.com>
2015-03-13 10:15:18 -0700

    Add test for single letter match

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -2,6 +2,18 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-        
+        function testSingleLetterMatch()
+        {
+            //Arrange
+            $test_single_letter_match = new RepeatCounter;
+            $string = "a";
+            $search = "a";
+
+            //Act
+            $result = $test_single_letter_match->countRepeats($string, $search);
+
+            //Assert
+            $this->assertEquals("Repeats: 1", $result);
+        }
     }
 ?>
