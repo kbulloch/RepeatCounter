3611e487b8ab4e7546c3ec8339c61364f3795dfa
Kyle <student@epicodus.com>
2015-03-13 11:31:58 -0700

    Add comments to app.php

--- app/app.php
+++ app/app.php
@@ -3,12 +3,15 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/RepeatCounter.php";
 
+    //instantiate new silex object
     $app = new Silex\Application();
 
+    //make sure twig is pointed at views directory
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__."/../views"
     ));
 
+    //set up the home page route
     $app->get("/", function() use ($app) {
         return $app['twig']->render('form.twig');
     });
