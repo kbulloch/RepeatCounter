9b12b83b5a482df24b141b92a2465b704fcdee04
Kyle <student@epicodus.com>
2015-03-13 13:08:39 -0700

    Change variable names to be more explicit

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -2,24 +2,27 @@
 
     class RepeatCounter
     {
-        function countRepeats($main, $query)
+        function countRepeats($phrase, $query)
         {
+            //trim whitespace
+            $query = trim($query);
+
             //make parameters globals so they can be used in the results page
-            $GLOBALS['the_string'] = $main;
+            $GLOBALS['the_string'] = $phrase;
             $GLOBALS['the_query'] = $query;
 
             //ignore case sensitivity
-            $main = strtolower($main);
+            $phrase = strtolower($phrase);
             $query = strtolower($query);
 
             //seperate words delimited by spaces into an array
-            $main_words = explode(" ", $main);
+            $phrase_words = explode(" ", $phrase);
 
             //keep track of number of repeats
             $num_repeats = 0;
 
             //look at each word one by one for a match
-            foreach($main_words as $my_word){
+            foreach($phrase_words as $my_word){
                 //increment repeat count when match found
                 if($query == $my_word){
                     ++$num_repeats;
