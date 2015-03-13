7924ab6a5a5ac9a06b390270b656d2f4f3bf0709
Kyle <student@epicodus.com>
2015-03-13 11:28:52 -0700

    Add twig register in app.php

--- app/app.php
+++ app/app.php
@@ -5,5 +5,9 @@
 
     $app = new Silex\Application();
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__."/../views"
+    ));
+
     return $app;
 ?>
