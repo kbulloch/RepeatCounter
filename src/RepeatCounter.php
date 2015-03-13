49c556bf5bff049ee328966ccac2086a29e90e3d
Kyle <student@epicodus.com>
2015-03-13 10:47:39 -0700

    Add comments to RepeatCounter.php

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,8 +4,20 @@
     {
         function countRepeats($main, $query)
         {
+            //ignore case sensitivity
             $main = strtolower($main);
             $query = strtolower($query);
+
+            //seperate words delimited by spaces into an array
+            $main_words = explode(" ", $main);
+
+            //keep track of number of repeats
+            $num_repeats = 0;
+
+            //look at each word one by one for a match
+            foreach($main_words as $my_word){
+                
+            }
 
             if($query == $main){
                 return "Repeats: 1";
